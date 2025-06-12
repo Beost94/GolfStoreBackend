31074d88d6c86d53a2cdc2a75c4458560b91b9ee
beost94 <Be.ostby@hotmail.com>
2025-06-12 18:00:27 +0200

    Update Brand.java

--- src/main/java/com/GolfStore/backend/model/Brand.java
+++ src/main/java/com/GolfStore/backend/model/Brand.java
@@ -1,22 +1,22 @@
-package com.GolfStore.backend.model;
+    package com.GolfStore.backend.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+    import jakarta.persistence.*;
+    import lombok.*;
 
-@Entity
-@Table(name = "brand")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+    @Entity
+    @Table(name = "brand")
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
 
-public class Brand {
+    public class Brand {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "brandid")
-    private Integer brandId;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "brandid")
+        private Integer brandId;
 
-    @Column(name = "brand_name", length = 50)
-    private String brandName;
-}
+        @Column(name = "brand_name", length = 50)
+        private String brandName;
+    }
